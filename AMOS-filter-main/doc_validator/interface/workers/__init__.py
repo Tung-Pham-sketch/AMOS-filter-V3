4ddb5a82b24c3a1d82f3511bf91b78fdc3574be4
Tung-Pham-sketch <tungpham23801@gmail.com>
2026-02-04 18:48:13 +0700

    Delete AMOS-filter-main directory

--- AMOS-filter-main/doc_validator/interface/workers/__init__.py
+++
@@ -1,8 +0,0 @@
-# doc_validator/interface/workers/__init__.py
-"""
-Background worker threads for the AMOSFilter GUI.
-"""
-
-from .processing_worker import ProcessingWorker
-
-__all__ = ["ProcessingWorker"]
--- AMOS-filter-main/run_gui.py
+++
@@ -1,8 +0,0 @@
-# run_gui.py
-import sys
-print(sys.path)
-
-from doc_validator.interface.main_window import launch
-
-if __name__ == "__main__":
-    launch()
